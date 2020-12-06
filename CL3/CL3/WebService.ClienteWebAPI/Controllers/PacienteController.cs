ef746aebbfd991a93db35d7c4f6ae8aeafb28944
i201820189 <Giomarlimocaldas1234@gmail.com>
2020-12-05 23:03:06 -0500

    First

---
+++ CL3/CL3/WebService.ClienteWebAPI/Controllers/PacienteController.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebService.DATA.Model;
+
+namespace WebService.ClienteWebAPI.Controllers
+{
+    public class PacienteController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public List<SP_LISTAPACIENTE_Result> listPaciente()
+        {
+            //System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+            HttpClient clienteHtpp = new HttpClient();
+            clienteHtpp.BaseAddress = new Uri("http://localhost:54100/");
+            var request = clienteHtpp.GetAsync("Api/Paciente").Result;
+           
+            //http://localhost:54100/Api/Paciente
+
+            var rs = request.Content.ReadAsStringAsync().Result;
+            var liPaciente = JsonConvert.DeserializeObject<List<SP_LISTAPACIENTE_Result>>(rs);
+
+            return liPaciente.ToList();
+
+        }
+
+        public ActionResult listadoPacientes()
+        {
+            return View(listPaciente());
+        }
+
+    }
+}
---
+++ CL3/CL3/WebService.WebApi/Controllers/PacienteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.DATA.Model;
+
+namespace WebService.WebApi.Controllers
+{
+    public class PacienteController : ApiController
+    {
+        CLINICAEntities1 db = new CLINICAEntities1();
+
+        [HttpGet]
+        public IEnumerable<SP_LISTAPACIENTE_Result> Get()
+        {
+            return db.SP_LISTAPACIENTE();
+        }
+        //LISTADO
+        [HttpGet]
+        public SP_LISTAPACIENTE_Result Get(int id)
+        {
+            var unPaciente = db.SP_LISTAPACIENTE().Where(p => p.IDE_PAC == id).FirstOrDefault();
+            return unPaciente;
+        }
+        //REGISTRA
+        [HttpPost]
+        public int Post(PACIENTE objP)
+        {
+            db.PACIENTE.Add(objP);
+            return db.SaveChanges();
+        }
+        //ACTUALIZA
+        [HttpPut]
+        public int Put(PACIENTE objP)
+        {
+            var unPaciente = db.SP_LISTAPACIENTE().Where(p => p.IDE_PAC == objP.IDE_PAC).FirstOrDefault();
+            
+            unPaciente.IDE_PAC = objP.IDE_PAC;
+            unPaciente.NOM_PAC = objP.NOM_PAC;
+            unPaciente.APE_PAC = objP.APE_PAC;
+            unPaciente.FNA_PAC = objP.FNA_PAC;
+            unPaciente.FON_PAC = objP.FON_PAC;
+
+            return db.SaveChanges();
+        }
+        //ELIMINAR
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            var unPaciente = db.PACIENTE.Where(p => p.IDE_PAC == id).FirstOrDefault();
+            db.PACIENTE.Remove(unPaciente);
+            return db.SaveChanges()>0; /*1 true 0 false*/
+        }
+    }
+}
