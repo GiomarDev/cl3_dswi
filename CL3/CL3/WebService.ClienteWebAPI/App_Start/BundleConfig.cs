5c394112b0a1354a94a45ce3623f302900705a40
i201820189 <Giomarlimocaldas1234@gmail.com>
2020-12-06 23:52:18 -0500

    Estilos Terminado

--- CL3/CL3/WebService.ClienteWebAPI/App_Start/BundleConfig.cs
+++ CL3/CL3/WebService.ClienteWebAPI/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace WebService.ClienteWebAPI
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/Estilos.css",
                       "~/Content/site.css"));
         }
     }
